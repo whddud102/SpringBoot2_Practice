cf59fcf70daaec7d41ac7b08cd829109f8212e71
JongYoung <whddud102@naver.com>
2020-05-04 20:48:52 +0900

    기능 추가
    SpringBootCommunityWebApplication.java
    - CommandLineRunner 를 구현
    - 애플리케이션 구동 후 특정 코드를 실행시킬 수 있다

--- src/main/java/com/web/SpringBootCommunityWebApplication.java
+++ src/main/java/com/web/SpringBootCommunityWebApplication.java
@@ -1,7 +1,17 @@
 package com.web;
 
+import com.web.domain.Board;
+import com.web.domain.BoardType;
+import com.web.domain.User;
+import com.web.repository.BoardRepository;
+import com.web.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SpringBootCommunityWebApplication {
@@ -9,5 +19,25 @@ public class SpringBootCommunityWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCommunityWebApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
+		return (args) -> {
+			User user = userRepository.save(User.builder()
+			.name("havi")
+			.password("test")
+			.email("havi@gmail.com")
+			.createdDate(LocalDateTime.now())
+			.build());
 
+			IntStream.rangeClosed(1,200).forEach(index -> boardRepository.save(Board.builder()
+			.title("게시글" + index)
+			.subTitle("순서" + index)
+			.content("콘텐츠")
+			.boardType(BoardType.free)
+			.createdDate(LocalDateTime.now())
+			.updateDate(LocalDateTime.now()
+			).user(user).build()));
+		};
+	}
 }

fe9654a940fabdb6d9d3df06dfa27bb67f1a37bc
JongYoung <whddud102@naver.com>
2020-05-06 14:31:45 +0900

    기능 추가
    SecurityConfig.java
    - 기본적인 시큐리티 설정 작성

--- src/main/java/com/web/config/SecurityConfig.java
+++ src/main/java/com/web/config/SecurityConfig.java
@@ -4,10 +4,44 @@ import com.web.oauth.ClientResources;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity      // 웹에서 security를 사용하도록 해주는 어노테이션
+public class SecurityConfig extends WebSecurityConfigurerAdapter {  // 설정을 최적화 하기 위해 ConfigurerAdapater를 상속
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 문자 인코딩 필터 생성
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+
+        http.authorizeRequests()
+                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                .and()
+                .formLogin()
+                .successForwardUrl("/board/list")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .and()
+                .addFilterBefore(filter, CsrfFilter.class)
+                .csrf().disable();
+    }
+
+    // 3개의 소셜 미디어 리소스 정보를 빈으로 등록
     @Bean
     @ConfigurationProperties("facebook")
     public ClientResources facebook() {
@@ -25,4 +59,6 @@ public class SecurityConfig {
     public ClientResources kakao() {
         return new ClientResources();
     }
+    
+    
 }
